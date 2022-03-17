19a37293c15644686cc3476f209e8e2f37aa7bc3
Berk Ozel <berkozel@protonmail.com>
2022-03-17 23:18:12 +0300

    minor blocks.h update

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -3,12 +3,12 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{"", "sb-music", 0, 12},
 	{"", "sb-loadavg", 60, 0},
-	{"", "sb-network", 1, 0},
-	{"", "sb-memory", 60, 0},
-	{"", "sb-battery", 3000, 0},
-	{"", "sb-datetime",	30,		0},
+	{" | ", "sb-network", 1, 0},
+	{" | ", "sb-memory", 60, 0},
+	{" | ", "sb-battery", 3000, 0},
+	{" | ", "sb-datetime",	30,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
+static char delim[] = "\0";
 static unsigned int delimLen = 5;
