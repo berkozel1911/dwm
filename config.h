ec924e272ede880440d4e40165ee543e21dd19cd
Berk Ozel <berkozel@protonmail.com>
2023-07-25 10:39:55 +0300

    Minor changes

--- config.h
+++ config.h
@@ -16,7 +16,7 @@ static const int user_bh            = 23;	    /* height of bar */
 static const unsigned int gappx     = 18;		/* fullgaps */
 
 /* fonts */
-static const char *fonts[]          = { "IBM Plex Mono:style=Semibold:size=10", "JoyPixels:pixelsize=15:antialias=true:autohint=true"};
+static const char *fonts[]  = { "BlexMono Nerd Font:style=Semibold:size=10", "JoyPixels:pixelsize=15:antialias=true:autohint=true"};
 
 /* colouring */
 static const char titlebar_bg[]  = "#2B4865";
@@ -41,21 +41,17 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray             = 1;     /* 0 means no systray */
 
 /* tags */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" }; 
+static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
 /* mostly used programs */
 static const char default_terminal[] = "alacritty";
 static const char web_browser[]      = "firefox";
 static const char file_manager[]     = "thunar";
-static const char *music_player[]    = {default_terminal, "-e", "/usr/bin/cmus"};
-static const char *newsboat[]	     = {default_terminal, "-e", "/usr/bin/newsboat"};
-static const char *calculator[]	     = {default_terminal, "-e", "qalc"};
-static const char *calendar[]	     = {default_terminal, "-e", "calcurse"};
+static const char music_player[]     = "spotify";
 static const char code_editor[]      = "codium";
-static const char program_launcher[] = "dmenu_run -fn 'Source Code Pro:style=Bold:size=11' -nf '#eae3d2' -sf '#000000' -sb '#bbe1fa'";
+static const char program_launcher[] = "dmenu_run -fn 'BlexMono Nerd Font:style=Bold:size=11' -nf '#eae3d2' -sf '#000000' -sb '#bbe1fa'";
 static const char screenlocker[]     = "slock";
-static const char ss_save[]  	 	 = "maim -s > ~/Pics/Screenshots/$(date +%d-%m-%Y-%H-%M-%S).png";
-static const char ss_clipboard[]	 = "maim -s | xclip -selection clipboard -t image/png";
+static const char take_ss[]	     = "flameshot gui";
 
 /* multimedia commands */
 static const char playpause[]   = "playerctl play-pause; pkill -RTMIN+12 dwmblocks";
@@ -76,7 +72,7 @@ static const Rule rules[] = {
 	 */
 	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
 	{ NULL,      NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
-	{ "Alacritty",  NULL,  NULL,					 0,					0,				  1,					 0,				 -1}
+	{ "Alacritty",  NULL,  NULL,           0,         0,          1,           0,        -1}
 };
 
 /* layout(s) */
@@ -107,45 +103,41 @@ static const Layout layouts[] = {
 
 
 static Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,            XK_d,      spawn,          SHCMD(program_launcher) },
-	{ MODKEY,            XK_Return, spawn,          SHCMD(default_terminal) },
-	{ MODKEY,			 XK_F1,	    spawn,	   		SHCMD(file_manager) },
-	{ MODKEY, 			 XK_F2,	    spawn, 	   		SHCMD(web_browser) },
-	{ 0, 			 XF86XK_Tools,          spawn, {.v = music_player} },
-	{ MODKEY, 			 XK_F3,     spawn, 	   		{.v = newsboat} },
-	{ MODKEY, 			 XK_F4,	    spawn, 	   		SHCMD(code_editor) },
-	{ 0, 				 XF86XK_Calculator,         spawn, {.v = calculator} },
-	{ MODKEY,			 XK_F5,	    spawn,	   		{.v = calendar} },
-	{ MODKEY, 			 XK_l,	    spawn, 	   		SHCMD(screenlocker) },
-	{ MODKEY,			 XK_q, 	    killclient, 	{0} },
-	{ MODKEY|ControlMask,XK_space,  layoutscroll,   {.i = +1 } },
-	{ MODKEY,			 XK_w,	    focusmon,       {.i = +1 } },
-	{ MODKEY|ShiftMask,	 XK_w,	    tagmon,	   		{.i = +1 } },
-	{ MODKEY,			 XK_Tab,	focusstack,     {.i = +1 } },
-	{ MODKEY, 			 XK_Right,  setmfact,       {.f = +0.05} },
-	{ MODKEY, 			 XK_Left,   setmfact,       {.f = -0.05} },
-	{ MODKEY|ShiftMask,	 XK_space,  togglefloating, {0} },
-	{ 0, 				 XF86XK_AudioPlay,          spawn, SHCMD(playpause)},
-	{ 0,				 XF86XK_AudioLowerVolume,   spawn, SHCMD(lowervolume) },
-	{ 0,				 XF86XK_AudioRaiseVolume,   spawn, SHCMD(raisevolume) },
-	{ 0,				 XF86XK_AudioMute,   	   	spawn, SHCMD(audiomute) },
-	{ 0,				 XK_Print,  spawn,	   		SHCMD(ss_clipboard) },
-	{ ShiftMask,		 XK_Print,	spawn,			SHCMD(ss_save) },
-	{ MODKEY, 			 XK_F11,	spawn,	   		SHCMD(mediaprev) },
-	{ MODKEY,			 XK_F12,    spawn, 	   		SHCMD(medianext) },
-	{ MODKEY,			 XK_space,  spawn,	   		SHCMD(flip_kbl) },
-	{ MODKEY,                       XK_s,      		togglesticky,   {0} },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
-	{ MODKEY|ShiftMask,             XK_r,      self_restart,   {0} },
-};
+	/* modifier                     key                         function        argument */
+	{ MODKEY,                       XK_d,                       spawn,          SHCMD(program_launcher) },
+	{ MODKEY,                       XK_Return,                  spawn,          SHCMD(default_terminal) },
+	{ MODKEY,			XK_F1,                      spawn,          SHCMD(file_manager) },
+	{ MODKEY, 			XK_F2,                      spawn,          SHCMD(web_browser) },
+	{ 0,                            XF86XK_Tools,               spawn,          SHCMD(music_player) },
+	{ 0,                            XK_Print,                   spawn,          SHCMD(take_ss) },
+	{ MODKEY,                       XK_F4,                      spawn,          SHCMD(code_editor) },
+	{ MODKEY,                       XK_l,                       spawn,          SHCMD(screenlocker) },
+	{ MODKEY,                       XK_q,                       killclient,     {0} },
+	{ MODKEY|ControlMask,           XK_space,                   layoutscroll,   {.i = +1 } },
+	{ MODKEY,                       XK_w,                       focusmon,       {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_w,                       tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_Tab,                     focusstack,     {.i = +1 } },
+	{ MODKEY,                       XK_Right,                   setmfact,       {.f = +0.05} },
+	{ MODKEY,                       XK_Left,                    setmfact,       {.f = -0.05} },
+	{ MODKEY|ShiftMask,             XK_space,                   togglefloating, {0} },
+	{ 0,                            XF86XK_AudioPlay,           spawn,          SHCMD(playpause)},
+	{ 0,                            XF86XK_AudioLowerVolume,    spawn,          SHCMD(lowervolume) },
+	{ 0,                            XF86XK_AudioRaiseVolume,    spawn,          SHCMD(raisevolume) },
+	{ 0,                            XF86XK_AudioMute,           spawn,          SHCMD(audiomute) },
+	{ MODKEY,                       XF86XK_AudioPrev,           spawn,          SHCMD(mediaprev) },
+	{ MODKEY,                       XF86XK_AudioNext,           spawn,          SHCMD(medianext) },
+	{ MODKEY,                       XK_space,                   spawn,          SHCMD(flip_kbl) },
+	{ MODKEY,                       XK_s,                       togglesticky,   {0} },
+        { MODKEY|ShiftMask,             XK_q,                       quit,           {0} },
+	{ MODKEY|ShiftMask,             XK_r,                       self_restart,   {0} },
+	TAGKEYS(                        XK_1,                       0)
+	TAGKEYS(                        XK_2,                       1)
+	TAGKEYS(                        XK_3,                       2)
+	TAGKEYS(                        XK_4,                       3)
+	TAGKEYS(                        XK_5,                       4)
+	TAGKEYS(                        XK_6,                       5)
+	TAGKEYS(                        XK_7,                       6)
+	};
 
 /* button definitions */
 /* click can be ClkTagBar, ClkLtSymbol, ClkStatusText, ClkWinTitle, ClkClientWin, or ClkRootWin */
@@ -159,7 +151,7 @@ static Button buttons[] = {
 
 /* disabled or unused variables but required for compilation */
 
-static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
+static char dmenumon[2]       = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run" };
 
 static const int vertpad            = 0;       /* vertical padding of bar */
--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -1,12 +1,10 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"", "sb-music", 0, 12},
 	{"", "sb-loadavg", 60, 0},
 	{"|🌡", "echo $(sensors | grep 'Package id 0' | awk '{ print $4}' | cut -c 2-3)°C", 60, 0},
 	{"|", "sb-network", 1, 0},
 	{"|", "sb-memory", 60, 0},
-	{"|", "sb-battery", 3000, 0},
 	{"|", "echo 🌍 $(setxkbmap -query | grep layout | awk '{print $2}')",0 , 13},
 	{"|", "sb-datetime",	30,		0},
 };
