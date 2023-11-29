ea9c751bb25524afb2491cc972dd1629f6e060cf
Berk Özel <berkozel@protonmail.com>
2023-11-29 15:07:06 +0300

    Reconstruction

--- config.h
+++ config.h
@@ -7,16 +7,16 @@
 #define STATUSBAR "dwmblocks"
 
 /* common dwm appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 0;       /* snap pixel */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int snap      = 0;       	/* snap pixel */
 static const int swallowfloating    = 1;        /* 1 means swallow floating windows by default */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const int user_bh            = 23;	    /* height of bar */
-static const unsigned int gappx     = 18;		/* fullgaps */
+static const int user_bh            = 23;	/* height of bar */
+static const unsigned int gappx     = 18;	/* fullgaps */
 
 /* fonts */
-static const char *fonts[]  = { "BlexMono Nerd Font:style=Semibold:size=10", "JoyPixels:pixelsize=15:antialias=true:autohint=true"};
+static const char *fonts[]  = { "MartianMono Nerd Font:style=Medium:size=10", "JoyPixels:pixelsize=15:antialias=true:autohint=true"};
 
 /* colouring */
 static const char titlebar_bg[]  = "#2B4865";
